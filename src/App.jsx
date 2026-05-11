f2790d62d25d11775a78df18d386a5e2df81a841
ImamHossain22 <akib.chowdhury64@gmail.com>
2026-05-11 23:38:08 +0600

    banner card added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Banner from './components/Banner/Banner'
 import NavBar from './components/NavBar/NavBar'
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
   return (
     <>
       <div className='px-20'>
-      <NavBar></NavBar>
+        <NavBar></NavBar>
+        <Banner></Banner>
       </div>
       
     </>
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,42 @@
+
+import vector from '../../assets/vector1.png'
+
+const Banner = () => {
+    return (
+        <div className="flex gap-5 mt-20 items-center">
+
+
+            <div className=" relative overflow-hidden rounded-xl flex items-center flex-row justify-center w-full h-64 bg-linear-to-r from-[#6e50e7] to-[#8d72e6] ">
+
+                <img className=" absolute top-0 left-1
+        " src={vector} alt="no" />
+
+                <img className=" absolute top-0 right-1 rotate-180
+        " src={vector} alt="no" />
+
+                <div className="text-center relative z-10 text-white">
+                    <h3 className="font-bold text-3xl">In-Progress</h3>
+                    <h1 className="font-bold text-7xl">0</h1>
+                </div>
+            </div>
+            <div className=" relative overflow-hidden rounded-xl flex items-center flex-row justify-center w-full h-64 bg-linear-to-r from-[#41b6a0] to-[#92dae5] ">
+
+                <img className=" absolute top-0 left-1
+        " src={vector} alt="no" />
+
+                <img className=" absolute top-0 right-1 rotate-180
+        " src={vector} alt="no" />
+
+                <div className="text-center relative z-10 text-white">
+                    <h3 className="font-bold text-3xl">Resolve</h3>
+                    <h1 className="font-bold text-7xl">0</h1>
+                </div>
+            </div>
+
+
+
+        </div>
+    );
+};
+
+export default Banner;
