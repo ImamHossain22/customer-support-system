b3b0d108a4a3100702a1f117e1989be9a89a369e
ImamHossain22 <akib.chowdhury64@gmail.com>
2026-05-11 22:01:43 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import NavBar from './components/NavBar/NavBar'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='px-20'>
+      <NavBar></NavBar>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,46 @@
+
+
+const NavBar = () => {
+
+    const navItems = <>
+
+        <li><a>Home</a></li>
+        <li><a>FAQ</a></li>
+        <li><a>Changelog</a></li>
+        <li><a>Blog</a></li>
+        <li><a>Download</a></li>
+        <li><a>Contact</a></li>
+        <button className="btn   text-white rounded-md px-5 bg-linear-to-r from-[#6c29e0] to-[#9470cf] hover:from-[#6D28D9] hover:to-[#5B21B6] shadow-md">
+      <span className="text-lg">+</span>
+      New Ticket
+    </button>
+    </>
+
+    return (
+        <div>
+            <div className="navbar  bg-base-100 shadow-sm">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                        </div>
+                        <ul
+                            tabIndex="-1"
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                            {navItems}
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost text-xl">CS — Ticket System</a>
+                </div>
+                <div className="navbar-end hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {navItems}
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
